8d27c984c3ddccd10faa1a09c474f34c6e23be5c
alfredasare <alfredasare101@gmail.com>
2023-06-03 22:16:40 +0000

    Add tests

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import Home from "@/app/page";
+
+describe('Home', function () {
+    it('renders Get started paragraph', () => {
+        render(<Home />)
+
+        const paragraph = screen.getByText(/get started by editing/i)
+
+        expect(paragraph).toBeInTheDocument();
+    })
+
+    it('has a link to the NextJS docs', () => {
+        render(<Home />)
+
+        const docsLink = screen.getByRole('link', { name: /Docs/i });
+        expect(docsLink).toHaveAttribute('href', 'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app');
+        expect(docsLink).toHaveAttribute('target', '_blank');
+        expect(docsLink).toHaveAttribute('rel', 'noopener noreferrer');
+    })
+});
